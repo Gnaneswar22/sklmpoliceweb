4d47c7239a050390e89135576097da46271a3a29
gnaneswar22 <gnanesh847@gmail.com>
2024-11-11 14:04:57 +0530

    commit-33

--- recent_activity.php
+++ recent_activity.php
@@ -7,6 +7,17 @@ class RecentActivity {
     public function __construct($conn) {
         $this->conn = $conn;
     }
+    public function addActivity($text, $link = '', $is_important = false) {
+        $text = $this->conn->real_escape_string($text);
+        $link = $this->conn->real_escape_string($link);
+        $is_important = $is_important ? 1 : 0;
+    
+        $sql = "INSERT INTO recent_activities (activity_text, activity_link, is_important) 
+                VALUES ('$text', '$link', $is_important)";
+        
+        return $this->conn->query($sql);
+    }
+    
 
     public function getRecentActivities() {
         $sql = "SELECT activity_text, activity_link FROM recent_activities ORDER BY created_at DESC LIMIT 10";
